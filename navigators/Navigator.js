7c5825e2849987d77daaf4af1b30d930b41ec4c7
Emil Lehtonen <emil.lehikoinen@outlook.com>
2023-10-02 09:55:34 +0300

    merge completed

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -7,34 +7,34 @@ import {MainContext} from '../contexts/MainContext';
 import {Icon} from '@rneui/themed';
 import FeedPage from '../views/FeedPage';
 import Upload from '../views/Upload';
+import Profile from '../views/Profile';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const Tabscreen = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
+    <Tab.Navigator screenOptions={{activeTintColor: '#FF385C', headerShown: false}}>
       <Tab.Screen
         name="Feed"
         component={FeedPage}
         options={{
-          tabBarIcon: ({color}) => <Icon name="home" color={color} />,
+          tabBarIcon: ({color}) => <Icon name="home" color={'#FF385C'} />,
           tabBarVisible: false, // this hides the tab bar only for this screen
         }}
       />
-      {/* <Tab.Screen
-        name="Profile"
-        component={Profile}
-        options={{
-          tabBarIcon: ({color}) => <Icon name="person" color={color} />,
-        }}
-      />
-      */}
+      { <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarIcon: ({color}) => <Icon name="person" color={'#FF385C'} />,
+          }}
+      /> }
       <Tab.Screen
         name="Upload"
         component={Upload}
         options={{
-          tabBarIcon: ({color}) => <Icon name="cloud-upload" color={color} />,
+          tabBarIcon: ({color}) => <Icon name="cloud-upload" color={'#FF385C'} />,
         }}
       />
     </Tab.Navigator>
