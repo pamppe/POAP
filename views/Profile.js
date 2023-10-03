c913295869632a8ca7747f8a20b9bd5ffc37b141
KimRautiainen <kim.rautiainen@hotmail.com>
2023-10-03 11:54:02 +0300

    load avatar fixed on avatar change

--- views/Profile.js
+++ views/Profile.js
@@ -1,7 +1,7 @@
-import { useContext, useState, useEffect } from 'react';
-import { MainContext } from '../contexts/MainContext';
+import {useContext, useState, useEffect} from 'react';
+import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Icon } from '@rneui/themed';
+import {Icon} from '@rneui/themed';
 import {
   ScrollView,
   TouchableOpacity,
@@ -9,21 +9,21 @@ import {
   Image,
   Text,
   Platform,
-  KeyboardAvoidingView, Alert
+  KeyboardAvoidingView,
+  Alert,
 } from 'react-native';
 import PropTypes from 'prop-types';
-import { useTag, useMedia } from '../hooks/ApiHooks';
-import { placeholderImage } from '../utils/app-config';
+import {useTag, useMedia} from '../hooks/ApiHooks';
+import {placeholderImage} from '../utils/app-config';
 import ProfileForm from '../components/ProfileForm';
 import * as ImagePicker from 'expo-image-picker';
 import {mediaUrl} from '../utils/app-config';
 
-
-const Profile = ({ navigation }) => {
-  const { setIsLoggedIn, user } = useContext(MainContext);
+const Profile = ({navigation}) => {
+  const {setIsLoggedIn, user} = useContext(MainContext);
   const [avatar, setAvatar] = useState(placeholderImage);
-  const { postMedia } = useMedia();
-  const {postTag,getFilesByTag} = useTag();
+  const {postMedia} = useMedia();
+  const {postTag, getFilesByTag} = useTag();
 
   const logOut = async () => {
     try {
@@ -37,10 +37,13 @@ const Profile = ({ navigation }) => {
   const handleAvatarPress = async () => {
     const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (status !== 'granted') {
-      Alert.alert('Sorry', 'We need camera roll permissions to make this work!');
+      Alert.alert(
+        'Sorry',
+        'We need camera roll permissions to make this work!',
+      );
       return;
     }
-    let result = await ImagePicker.launchImageLibraryAsync({
+    const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 3],
@@ -48,11 +51,12 @@ const Profile = ({ navigation }) => {
     });
 
     if (!result.canceled && result.assets && result.assets.length > 0) {
-      let avatarUri = result.assets[0].uri;
-      setAvatar({ uri: avatarUri });
+      const avatarUri = result.assets[0].uri;
+      setAvatar({uri: avatarUri});
       await uploadAvatar(avatarUri);
+      await loadAvatar();
     }
-  }
+  };
   const uploadAvatar = async (uri) => {
     const formData = new FormData();
 
@@ -70,14 +74,19 @@ const Profile = ({ navigation }) => {
       const token = await AsyncStorage.getItem('userToken');
       const response = await postMedia(formData, token);
       console.log('Server Response:', response);
-      console.log('user from mainContext: ', user)
+      console.log('user from mainContext: ', user);
 
-        await postTag({ file_id: response.file_id, tag: 'avatar_' + user.user_id }, token);
-        Alert.alert('Avatar uploaded successfully!');
-
+      await postTag(
+        {file_id: response.file_id, tag: 'avatar_' + user.user_id},
+        token,
+      );
+      Alert.alert('Avatar uploaded successfully!');
     } catch (error) {
       console.error('Error uploading avatar:', error);
-      Alert.alert('Upload error', 'There was an error uploading the avatar. Please try again later.');
+      Alert.alert(
+        'Upload error',
+        'There was an error uploading the avatar. Please try again later.',
+      );
     }
   };
   const loadAvatar = async () => {
@@ -99,85 +108,92 @@ const Profile = ({ navigation }) => {
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={{ flex: 1 }}
+      style={{flex: 1}}
     >
-    <ScrollView
-      style={{ backgroundColor: 'black', flexGrow: 1}}
-      keyboardShouldPersistTaps="handled"
-    >
-      <View style={{ alignItems: 'center' }}>
-        <TouchableOpacity onPress={handleAvatarPress}>
-        <Image
-          source={{uri: avatar}}
-          style={{
-            width: 100,
-            height: 100,
-            borderRadius: 50,
-            marginTop: 20,
-            alignSelf: 'center',
-          }}
-        />
-        </TouchableOpacity>
-        <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 10, color: 'white' }}>
-          {user.username}
-        </Text>
-        <Text style={{ fontSize: 16, color: 'white', marginBottom: 10 }}>
-          {user.full_name}
-        </Text>
-      </View>
-      <View style={{ marginTop: 20, paddingHorizontal: 20 }}>
-        <TouchableOpacity
-          style={{
-            backgroundColor: '#FF385C',
-            paddingVertical: 15,
-            borderRadius: 10,
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-          onPress={() => {
-            navigation.navigate('My files');
-          }}
-        >
+      <ScrollView
+        style={{backgroundColor: 'black', flexGrow: 1}}
+        keyboardShouldPersistTaps="handled"
+      >
+        <View style={{alignItems: 'center'}}>
+          <TouchableOpacity onPress={handleAvatarPress}>
+            <Image
+              source={{uri: avatar}}
+              style={{
+                width: 100,
+                height: 100,
+                borderRadius: 50,
+                marginTop: 20,
+                alignSelf: 'center',
+              }}
+            />
+          </TouchableOpacity>
           <Text
             style={{
-              color: 'white',
-              fontSize: 16,
+              fontSize: 20,
               fontWeight: 'bold',
-              marginRight: 10,
+              marginTop: 10,
+              color: 'white',
             }}
           >
-            My files
+            {user.username}
           </Text>
-          <Icon name="storage" color="white" />
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{
-            backgroundColor: '#FF385C',
-            paddingVertical: 15,
-            borderRadius: 10,
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginTop: 10,
-          }}
-          onPress={logOut}
-        >
-          <Text
+          <Text style={{fontSize: 16, color: 'white', marginBottom: 10}}>
+            {user.full_name}
+          </Text>
+        </View>
+        <View style={{marginTop: 20, paddingHorizontal: 20}}>
+          <TouchableOpacity
             style={{
-              color: 'white',
-              fontSize: 16,
-              fontWeight: 'bold',
-              marginRight: 10,
+              backgroundColor: '#FF385C',
+              paddingVertical: 15,
+              borderRadius: 10,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            onPress={() => {
+              navigation.navigate('My files');
             }}
           >
-            Log out!
-          </Text>
-          <Icon name="logout" color="white" />
-        </TouchableOpacity>
-      </View>
-      <ProfileForm user={user} />
-    </ScrollView>
+            <Text
+              style={{
+                color: 'white',
+                fontSize: 16,
+                fontWeight: 'bold',
+                marginRight: 10,
+              }}
+            >
+              My files
+            </Text>
+            <Icon name="storage" color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#FF385C',
+              paddingVertical: 15,
+              borderRadius: 10,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginTop: 10,
+            }}
+            onPress={logOut}
+          >
+            <Text
+              style={{
+                color: 'white',
+                fontSize: 16,
+                fontWeight: 'bold',
+                marginRight: 10,
+              }}
+            >
+              Log out!
+            </Text>
+            <Icon name="logout" color="white" />
+          </TouchableOpacity>
+        </View>
+        <ProfileForm user={user} />
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 };
